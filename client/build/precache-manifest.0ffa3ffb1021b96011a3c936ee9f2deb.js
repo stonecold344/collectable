e74f2f093ef394cd88f62133988ed10ad1a4ab05
stonecold344 <49286565+stonecold344@users.noreply.github.com>
2020-11-19 19:51:28 +0200

    Add files via upload

---
+++ client/build/precache-manifest.0ffa3ffb1021b96011a3c936ee9f2deb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196f379ecd6ed72c8bfb2e0f4f3d6e9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09624f75576b6804c499",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "a14a6613bea94a0619d8",
+    "url": "/static/css/main.4cc45adb.chunk.css"
+  },
+  {
+    "revision": "09624f75576b6804c499",
+    "url": "/static/js/2.28b83f87.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.28b83f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14a6613bea94a0619d8",
+    "url": "/static/js/main.858e45ca.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "e49c3ffb72e3a514ef65507514d0f780",
+    "url": "/static/media/cart-bg.e49c3ffb.jpg"
+  },
+  {
+    "revision": "7697f6b7d1dedf834857e85f09da8d3b",
+    "url": "/static/media/error-bg.7697f6b7.jpg"
+  },
+  {
+    "revision": "e0662f22e4a30499873c47d4b8ad2928",
+    "url": "/static/media/home-bg.e0662f22.jpg"
+  },
+  {
+    "revision": "40df68f70b0e853ef41f45c552437e9a",
+    "url": "/static/media/product-details-bg.40df68f7.jpg"
+  },
+  {
+    "revision": "e0c2a4f8201e160f0e7fa5f4db88024d",
+    "url": "/static/media/profile-bg.e0c2a4f8.jpg"
+  },
+  {
+    "revision": "053d0e0e6a047192ab76c94f47355cdd",
+    "url": "/static/media/signin-bg.053d0e0e.jpg"
+  },
+  {
+    "revision": "0ec8f47977aeb67c459a119a95598c95",
+    "url": "/static/media/signup-bg.0ec8f479.jpg"
+  },
+  {
+    "revision": "3c59bb70a53cdeaef863ce0ec7ba7bb0",
+    "url": "/static/media/store-bg.3c59bb70.jpg"
+  }
+]);
